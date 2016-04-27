c5492a83b0b54645219418ccf949fae9858acfac
Ezra Raez <ezRAez10@gmail.com>
2016-04-26 19:51:42 -0700

    halfway through chap 4

--- clojure_brave_true/clojure-noob/src/clojure_noob/core.clj
+++ clojure_brave_true/clojure-noob/src/clojure_noob/core.clj
@@ -135,3 +135,22 @@
       (if (= iteration 100)
         iteration
         (recur (inc iteration))))))
+
+(def human-consumption [8.1 7.3 6.6 5.0])
+(def critter-consumption [0.0 0.2 0.3 1.1])
+(defn unify-diet-data
+  [human critter]
+  {:human human
+   :critter critter})
+
+(def sum #(reduce + %))
+(def avg #(/ (sum %) (count %)))
+(defn stats [numbers]
+  (map #(% numbers) [sum count avg]))
+
+(def identities
+  [{:alias "Batman" :real "Bruce Wayne"}
+   {:alias "Spider-Man" :real "Peter Parker"}
+   {:alias "Santa" :real "Your mom"}
+   {:alias "Easter Bunny" :real "Your dad"}])
+
