a9680a599919e5c5cf3cf8f20d6e88951929f984
Ezra Raez <ezRAez10@gmail.com>
2016-05-13 12:29:32 -0700

    add ui for battle

--- reactjs_program/github_battle/app/components/confirm_battle.js
+++ reactjs_program/github_battle/app/components/confirm_battle.js
@@ -1,4 +1,9 @@
-var React = require('react');
+var React              = require('react');
+var PropTypes          = React.PropTypes;
+var styles             = require('../styles');
+var Link               = require('react-router').Link;
+var UserDetails        = require('./user_details');
+var UserDetailsWrapper = require('./user_details_wrapper');
 
 function puke(obj) {
   return <pre>{JSON.stringify(obj, null, ' ')}</pre>
@@ -7,7 +12,40 @@ function puke(obj) {
 function ConfirmBattle(props) {
   return props.isLoading === true
     ? <p> LOADING! </p>
-    : <div> CONFIRM BATTLE!: {puke(props)} </div>
+    : <div className="jumbotron col-sm-12 text-center">
+        <h1>Confirm Players</h1>
+        <div className="col-sm-8 col-sm-offset-2">
+          <UserDetailsWrapper header="Player One">
+            <UserDetails info={props.playersInfo[0]} />
+          </UserDetailsWrapper>
+          <UserDetailsWrapper header="Player Two">
+            <UserDetails info={props.playersInfo[1]} />
+          </UserDetailsWrapper>
+        </div>
+        <div className="col-sm-8 col-sm-offset-2">
+          <div className="col-sm-12" style={styles.space}>
+            <button className="btn btn-lg btn-success"
+                    type="button"
+                    onClick={props.onInitiateBattle}>
+              Initiate Battle
+            </button>
+          </div>
+          <div className="col-sm-12" style={styles.space}>
+            <Link to='/playerOne'>
+              <button type="button"
+                      className="btn btn-lg btn-danger">
+                Reselect Players
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+}
+
+ConfirmBattle.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired,
+  playersInfo: PropTypes.array.isRequired
 }
 
 module.exports = ConfirmBattle;
