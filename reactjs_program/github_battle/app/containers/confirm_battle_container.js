3280efa38d7172f155c7913503504904f76393e6
Ezra Raez <ezRAez10@gmail.com>
2016-05-13 00:49:13 -0700

    add get req to github for user data

--- reactjs_program/github_battle/app/containers/confirm_battle_container.js
+++ reactjs_program/github_battle/app/containers/confirm_battle_container.js
@@ -1,5 +1,6 @@
 var React         = require('react');
 var ConfirmBattle = require('../components/confirm_battle');
+var gitHubHelpers = require('../utils/github_helpers');
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
@@ -14,6 +15,13 @@ var ConfirmBattleContainer = React.createClass({
   componentDidMount: function() {
     var query = this.props.location.query;
     // Fech info from GitHub then update state
+    gitHubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+    .then(function(players) {
+      this.setState({
+        isLoading: false,
+        playersInfo: [players[0], players[1]]
+      })
+    }.bind(this));
   },
   render: function() {
     return (
